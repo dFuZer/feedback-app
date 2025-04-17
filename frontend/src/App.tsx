a5cbb06a59b7536d4b21da29be5c1c6a43fb3bd2
Elias <eliaslvr59@gmail.com>
2025-04-17 14:57:55 +0200

    commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./components/Main/Main";
+import './assets/styles/index.scss'
 
-function App() {
-    return <div>Hello World</div>;
+const App: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Main />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Main() {
-  return (
-    <div>Main</div>
-  )
-}
+const Main: React.FC = () => {
+    const [showForm, setShowForm] = useState<boolean>(false);
+    const [message, setMessage] = useState<string>('');
+    const [category, setCategory] = useState<string>('');
+    const [title, setTitle] = useState<string>('');
 
-export default Main
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (!message || category == '' || !title ) {
+            alert('Veuillez entrer un titre, un message et séléctionner une catégorie avant de soumettre.');
+        } else {   
+            alert('Votre feedback a été soumis !');
+            setShowForm(false)
+            setMessage(''); 
+            setCategory('');
+            setTitle('');
+        }
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(event.target.value);
+    };
+
+    const handleCategory = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setCategory(event.target.value);
+    };
+
+    const handleTitle = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setTitle(event.target.value);
+    };
+
+    return (
+        <div>
+            {!showForm && (
+                <button className='firstButton' onClick={() => setShowForm(true)}>Afficher le formulaire</button>
+            )}
+
+            {showForm && (
+                <>
+                <div className='main'>
+            <h2>Envoyer votre feedback</h2>
+            <form onSubmit={handleSubmit}>
+
+                <input type="text" value={title} onChange={handleTitle} placeholder='Titre :' />
+                <label htmlFor="category">Choisir une catégorie :</label>
+                <select id="category" value={category} onChange={handleCategory}>
+                    <option value=""></option>
+                    <option value="a">A</option>
+                    <option value="b">B</option>
+                    <option value="c">C</option>
+                    <option value="d">D</option>
+                </select>
+
+                <textarea value={message} onChange={handleChange} placeholder="Écrivez votre message ici" rows={10}
+  />
+                <br />
+                <button className='secondButton' type="submit">Envoyer</button>
+            </form>
+            </div>
+            </>
+            )}
+        </div>
+    );
+};
+
+export default Main;
