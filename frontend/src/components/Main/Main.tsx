f4903f5133725625e0c84f42cb36b16802235bcb
Sharizhai <jade.besserve-moullahem@hotmail.fr>
2025-04-18 12:17:34 +0200

    Create bento grid component & add it to main + reorganize components on main + add feedbackdatas

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
-import Slider from '../Slider';
-import FeedbackForm from '../FeedbackForm';
-import SearchBar from '../Searchbar';
-import slidesArray from '../../data/slidesData.json';
 import { renderSlides } from '../../utils/renderSlides';
+import slidesArray from '../../data/slidesData.json';
+import FeedbackForm from '../FeedbackForm';
+import React, { useState } from 'react';
+import SearchBar from '../Searchbar';
+import BentoGrid from '../BentoGrid';
+import Slider from '../Slider';
 
 const Main: React.FC = () => {
     const [showForm, setShowForm] = useState<boolean>(false);
     const [filteredSlides, setFilteredSlides] = useState(slidesArray);
+
+    const getRandomSlides = (slides: typeof slidesArray, count: number) => {
+        const shuffled = [...slides].sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, count);
+    };
+
+    const randomSlides = getRandomSlides(filteredSlides, 5);
 
     const sliderOptions = {
         type: "carousel" as "carousel",
@@ -41,25 +49,27 @@ const Main: React.FC = () => {
                         Afficher le formulaire
                     </button>
                     <div className="sliderContainer">
-                        <SearchBar onSearch={onSearchButtonClick} />
                         <Slider 
-                            slides={renderSlides(filteredSlides)}
+                            slides={renderSlides(randomSlides)}
                             options={sliderOptions}
                             className="diverse-content-slider"
                         />
+                        <SearchBar onSearch={onSearchButtonClick} />
                     </div>
+                    <BentoGrid slides={filteredSlides} />
                 </>
             ) : (
                 <>
                     <FeedbackForm onClose={() => setShowForm(false)} />
                     <div className="sliderContainer">
-                        <SearchBar onSearch={onSearchButtonClick} />
                         <Slider 
-                            slides={renderSlides(filteredSlides)}
+                            slides={renderSlides(randomSlides)}
                             options={sliderOptions}
                             className="diverse-content-slider"
                         />
+                        <SearchBar onSearch={onSearchButtonClick} />
                     </div>
+                    <BentoGrid slides={filteredSlides} />
                 </>
             )}
         </div>
--- frontend/src/components/Searchbar.tsx
+++ frontend/src/components/Searchbar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import searchIcon from "../assets/icons/search.svg";
 import "../css/Searchbar.css";
 
